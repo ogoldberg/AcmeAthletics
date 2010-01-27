f8896b3a9efd29573af269b3ab281fd6341f1cbe
Oren Goldberg <ogoldberg@gmail.com>
2010-01-26 23:40:03 -0600

    added a user to bootstrap and fixed acmeathletes package name

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,14 +1,24 @@
+import org.apache.shiro.crypto.hash.Sha1Hash
+
 class BootStrap {
 
 
     def init = { servletContext ->
+        def user = new ShiroUser(username: "user123", passwordHash: new Sha1Hash("password").toHex())
+        user.addToPermissions("*:*")
+        user.save()
+
         development {
-            def d1 = new com.oren.AcmeAthletics.Department (
+            def d1 = new com.oren.acmeathletes.Department (
                 name: "Baseball Operations" )
 
         }
+
+
     }
     def destroy = {
-    }
 }
+}
+
+
 
