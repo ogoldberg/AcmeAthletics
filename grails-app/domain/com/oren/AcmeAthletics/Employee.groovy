a313372fcd7a48560b1a42a056d75ba50dd673ef
Oren Goldberg <ogoldberg@gmail.com>
2010-01-26 22:35:09 -0600

    Created employee domain and tests

---
+++ grails-app/domain/com/oren/AcmeAthletics/Employee.groovy
@@ -0,0 +1,7 @@
+package com.oren.AcmeAthletics
+
+class Employee {
+
+    static constraints = {
+    }
+}
---
+++ test/unit/com/oren/AcmeAthletics/EmployeeTests.groovy
@@ -0,0 +1,17 @@
+package com.oren.AcmeAthletics
+
+import grails.test.*
+
+class EmployeeTests extends GrailsUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
