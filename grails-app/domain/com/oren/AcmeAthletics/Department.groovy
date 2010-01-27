2aac9ef52948a5e815ce2de1a7350c33516d6291
Oren Goldberg <ogoldberg@gmail.com>
2010-01-26 23:08:31 -0600

    added attributes to employee and department class pointed them at each other

--- grails-app/domain/com/oren/AcmeAthletics/Department.groovy
+++ grails-app/domain/com/oren/AcmeAthletics/Department.groovy
@@ -1,6 +1,8 @@
 package com.oren.AcmeAthletics
 
 class Department {
+    String name
+    static hasMany=[employees:Employee]
 
     static constraints = {
     }
--- grails-app/domain/com/oren/AcmeAthletics/Employee.groovy
+++ grails-app/domain/com/oren/AcmeAthletics/Employee.groovy
@@ -2,6 +2,14 @@ package com.oren.AcmeAthletics
 
 class Employee {
 
+    String firstName
+    String lastName
+    Department department
+
+
     static constraints = {
+        firstName(blank:false)
+        lastName(blank:false)
+        department(blank:false)
     }
 }
