2127cb69dbb144de78e6ec88ce210334acb75f54
Oren Goldberg <ogoldberg@gmail.com>
2010-02-05 21:38:16 -0600

    added some attributes and notes to game.groovy

--- grails-app/domain/com/oren/acmeathletes/Game.groovy
+++ grails-app/domain/com/oren/acmeathletes/Game.groovy
@@ -4,8 +4,24 @@ class Game {
     def scaffold = true
     
     Date gameDate
+    Date gameTime
+
+    //TODO:Cubs games are premium price + $5
     String opponent
+
+    //TODO:Away games are not available through this system
     Boolean homeGame
+
+    Boolean ticketsAvailable
+
+    //TODO:opening day means premium price
+    Boolean openingDay
+    //TODO: Premium dates include Fri-Sun games in May-October
+
+
+
+
+
 
     static constraints = {
     }
