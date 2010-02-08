5e132892ddc19dc8199699160cdb80f128fe7daf
Oren Goldberg <ogoldberg@gmail.com>
2010-02-07 22:58:20 -0600

    created location controller and scaffold.

---
+++ test/unit/com/oren/acmeathletes/LocationControllerTests.groovy
@@ -0,0 +1,17 @@
+package com.oren.acmeathletes
+
+import grails.test.*
+
+class LocationControllerTests extends ControllerUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
