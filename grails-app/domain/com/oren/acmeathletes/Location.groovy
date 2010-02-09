9294648314bd6afd5818d0062385538c7df6ad9e
Oren Goldberg <ogoldberg@gmail.com>
2010-02-09 17:28:21 -0600

    changed prices from "Number" to "Double" added "allInclusive" boolean

--- grails-app/domain/com/oren/acmeathletes/Location.groovy
+++ grails-app/domain/com/oren/acmeathletes/Location.groovy
@@ -2,10 +2,12 @@ package com.oren.acmeathletes
 
 class Location {
     String name
-    Number regularPrice
-    Number premiumPrice
-    Boolean seatsAvaliable
+    Double regularPrice
+    Double premiumPrice
+    Boolean seatsAvailable = true
+    Boolean allInclusive = false
 
     static constraints = {
+       
     }
 }
