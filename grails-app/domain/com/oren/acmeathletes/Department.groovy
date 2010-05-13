ada6f1daf4379d2a2c6a27e73a8d53984aa6ee1a
oren <oren@oren-laptop.(none)>
2010-05-13 16:37:03 -0500

    removed vp attribute from department

--- grails-app/domain/com/oren/acmeathletes/Department.groovy
+++ grails-app/domain/com/oren/acmeathletes/Department.groovy
@@ -3,17 +3,15 @@ package com.oren.acmeathletes
 class Department {
     String name
     String phone
-    String vp
     static hasMany=[employees:Employee]
 
-    String toString {
+    String toString() {
         this.name
     }
 
     static constraints = {
         name(nullable:false, blank:false)
         phone()
-        vp()
         employees(blank:true) 
     }
 }
